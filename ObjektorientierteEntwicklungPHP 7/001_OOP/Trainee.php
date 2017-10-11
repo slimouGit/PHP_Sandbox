19b725e9afbb9ae72207a92ebf85490fde9ef76a
Salim Oussayfi <box@slimou.net>
2017-10-11 20:32:19 +0200

    ...

--- ObjektorientierteEntwicklungPHP 7/001_OOP/Trainee.php
+++ ObjektorientierteEntwicklungPHP 7/001_OOP/Trainee.php
@@ -8,13 +8,19 @@
         public $tutor;
         public $teacher;
         
-        public function printTrainee(){
+        public function printNameTrainee(){
             return $this->forename . " " . $this->surname;
         }
+        
+        public function printTrainee(){
+            return $this->forename . " " . $this->surname . "<br>" . "Abteilung: " . $this->department . ", Dauer: " . $this->intershipLength . " Wochen" . "<br>" . "Tutor: " . $this->tutor . ", Lehrer: " . $this->teacher;
+        }
     }
+    echo Trainee::POSITION;
+    echo "<br>";
     
     $fritz = new Trainee();
-    echo Trainee::POSITION;
+    
     $fritz->forename = "Fritz";
     $fritz->surname = "Baum";
     $fritz->intershipLength = 10;
@@ -22,5 +28,6 @@
     $fritz->tutor = "Meyer";
     $fritz->teacher = "Krone";
     
+    echo $fritz->printNameTrainee();
     echo $fritz->printTrainee();
     
