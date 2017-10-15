a4ff3fa7eb1a8d3eae5e0b913a9693b26914ab61
Salim Oussayfi <box@slimou.net>
2017-10-15 13:36:58 +0200

    ...

--- ObjektorientierteEntwicklungPHP 7/003_Bootsverleih/script/Boathire.php
+++ ObjektorientierteEntwicklungPHP 7/003_Bootsverleih/script/Boathire.php
@@ -23,19 +23,28 @@ class Boathire
         $name = $this->name;
         $boat = $this->boat;
 
-        echo "Boot " . $boat . " wurde um " . $checkInDate . " Uhr ausgeliehen";
+        //echo "Boot " . $boat . " wurde um " . $checkInDate . " Uhr ausgeliehen";
 
         $pdo = new PDO('mysql:host=localhost;dbname=bootsverleih;charset=utf8', 'root', '');
         $res = $pdo->prepare("INSERT INTO ausleihe (name, boat, checkIn) VALUES (:name, :boat, :checkIn)");
         $res->execute(array('name' => $name, 'boat' => $boat, 'checkIn' => $checkInDate));
+
+
     }
 
     public static function returnBoat($boat){
         $checkOutDate = date("h:i");
-        echo "Boot " . $boat . " wurde um " . $checkOutDate . " Uhr zurückgegeben";
+
+        echo "Boot " . $boat . " wurde um " . $checkOutDate . " Uhr zurückgegeben<br>";
+
         $pdo = new PDO('mysql:host=localhost;dbname=bootsverleih;charset=utf8', 'root', '');
         $res = $pdo->prepare("UPDATE ausleihe SET checkOut = :checkOut WHERE boat = :boat");
         $res->execute(array('boat' => $boat, 'checkOut' => $checkOutDate));
+
+        $sql = "SELECT * FROM ausleihe WHERE boat LIKE $boat";
+        foreach ($pdo->query($sql) as $row) {
+            echo "<br>Ausleihdaten:<br>" . "Ausgeliehen: " . $row['checkIn']." - Abgegeben: ".$row['checkOut'] ;
+        }
     }
 }
 
