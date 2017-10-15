9cc6a86d5c656c2a640b4900dabd4be5199f0a48
Salim Oussayfi <box@slimou.net>
2017-10-15 13:39:57 +0200

    ...

--- ObjektorientierteEntwicklungPHP 7/003_Bootsverleih/index.php
+++ ObjektorientierteEntwicklungPHP 7/003_Bootsverleih/index.php
@@ -7,6 +7,15 @@ include "script/Boathire.php";
 <head>
     <title>Bootsverleih</title>
     <meta charset="utf-8">
+    <style>
+        h2 {margin:0px;padding-top:10px}
+        #rentBoats {
+            margin-top: 20px;
+            padding-bottom: 20px;
+            border-top: 1px solid #000;
+            border-bottom: 1px solid #000;
+        }
+    </style>
 </head>
 <body>
 
@@ -42,4 +51,17 @@ if(isset($_GET['return'])) {
     $boat = $_POST['boat'];
     Boathire::returnBoat($boat);
 }
+
+
 ?>
+<div id="rentBoats">
+
+    <h2>vermietete Boote:</h2>
+    <?php
+$pdo = new PDO('mysql:host=localhost;dbname=bootsverleih;charset=utf8', 'root', '');
+$sql = "SELECT * FROM ausleihe WHERE checkOut like ''";
+foreach ($pdo->query($sql) as $row) {
+    echo "<br>Boot: " . $row['boat'] .  " ausgeliehen von: " . $row['name'] . " um " . $row['checkIn']." Uhr";
+}
+?>
+</div>
